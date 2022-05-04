c72e95b7c0074f0ff6c39f77ef31d191e1808564
aaqibb13 <aaqibb13@gmail.com>
2022-05-04 17:02:04 +0530

    Add executor for executing queries, settings to setup database and collections, constants to be used in arangodb initialization

---
+++ guinevere/database/arangodb/executor.go
@@ -0,0 +1,90 @@
+package arangodb
+
+import (
+	"context"
+	"github.com/arangodb/go-driver"
+)
+
+//ArangoDbQueryExecutor is the primary type for executing queries
+type QueryExecutorArangoDB struct {
+	DB driver.Database
+	db driver.Database
+	QueryExecutor
+}
+
+//Execute runs the specified query
+func (q *QueryExecutorArangoDB) Execute(queryText string, bindVars map[string]interface{}) ([]map[string]interface{}, error) {
+	if q.db == nil {
+		db, err := InitializeArangoDb()
+		if err != nil {
+			return nil, err
+		}
+		q.db = db
+		q.DB = db
+	}
+
+	ctx := driver.WithQueryCount(context.Background())
+	cursor, err := q.db.Query(ctx, queryText, bindVars)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close()
+
+	count := cursor.Count()
+	data := make([]map[string]interface{}, count)
+
+	idx := 0
+	for {
+		var doc map[string]interface{}
+		_, err := cursor.ReadDocument(ctx, &doc)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			// handle other errors
+		}
+
+		data[idx] = doc
+		idx++
+	}
+
+	return data, nil
+}
+
+//Execute runs the specified query and gets the full count from cursor
+func (q *QueryExecutorArangoDB) ExecuteWithCount(queryText string, bindVars map[string]interface{}) ([]map[string]interface{}, int64, error) {
+	if q.db == nil {
+		db, err := InitializeArangoDb()
+		if err != nil {
+			return nil, 0, err
+		}
+		q.db = db
+		q.DB = db
+	}
+
+	ctx := driver.WithQueryFullCount(context.Background())
+	cursor, err := q.db.Query(ctx, queryText, bindVars)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	defer cursor.Close()
+
+	count := cursor.Statistics().FullCount()
+	var data []map[string]interface{}
+
+	idx := 0
+	for {
+		var doc map[string]interface{}
+		_, err := cursor.ReadDocument(ctx, &doc)
+		if driver.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			// handle other errors
+		}
+
+		data = append(data, doc)
+		idx++
+	}
+
+	return data, count, nil
+}
